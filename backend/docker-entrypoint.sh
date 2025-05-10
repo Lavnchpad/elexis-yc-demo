decf4d7174bc517db56de922d954e530c4fb9e56
adityahp241099 <adityahp241099@outlook.com>
2025-05-11 00:31:52 +0530

    WIP Dockerfile

---
+++ backend/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+export DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE}
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+gunicorn elexis_dashboard.wsgi:application --bind ${HOST}:${PORT} --log-level=debug \
+                          --timeout=0 \
+                          --access-logfile=-\
+                          --log-file=-
