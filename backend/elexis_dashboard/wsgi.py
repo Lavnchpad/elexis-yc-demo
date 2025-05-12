3ebd084b98722194efd4920ed11faba89dde1c9b
adityahp241099 <adityahp241099@outlook.com>
2025-05-12 12:48:59 +0530

    setting the env variable before listener import

--- backend/elexis_dashboard/wsgi.py
+++ backend/elexis_dashboard/wsgi.py
@@ -12,10 +12,12 @@ import threading
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elexis_dashboard.settings')
+
 from elexis.sqs_consumer import start_sqs_consumer
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elexis_dashboard.settings')
+
 threading.Thread(target=start_sqs_consumer, daemon=True).start()
 
 application = get_wsgi_application()
