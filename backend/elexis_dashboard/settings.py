de6241b34ccb25babb149db3b2f12e39f9d10ae1
adityahp241099 <adityahp241099@outlook.com>
2025-09-04 03:57:54 +0530

    Modifying settings.py

--- backend/elexis_dashboard/settings.py
+++ backend/elexis_dashboard/settings.py
@@ -107,12 +107,12 @@ WSGI_APPLICATION = 'elexis_dashboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
